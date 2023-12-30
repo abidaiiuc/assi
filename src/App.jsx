791a42223eab7d61acd17422ea69b7f636ae0aac
Abida Sultana <abidaiiuc826@gmail.com>
2023-12-30 12:40:18 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import AddEntry from "./components/AddEntry";
 import Header from "./components/Header";
-import AddEntry from "./components/addEntry";
 import Listing from "./components/Listing";
+
 function App() {
   return (
     <div>
       <Header />
-      <AddEntry />
-      <Listing />
+      {/* <AddEntry /> */}
+      {/* <Listing /> */}
+      <AddEntry></AddEntry>
+      <Listing></Listing>
+
     </div>
   );
 }
